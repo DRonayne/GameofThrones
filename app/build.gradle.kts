df0760545f779c192464b0a711027bf79a8a9d80
Darach Ronayne <darachronayne@gmail.com>
2025-09-30 13:50:00 +0100

    Theming

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,9 +108,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.compose.material3.adaptive.layout)
     implementation(libs.androidx.compose.material3.adaptive.navigation)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
 
     // Window
     implementation(libs.androidx.window)
